4d85c9daf34be5ee65faa5ae6f0cddaae6901d9c
sumittshrma <ss7675088@gmail.com>
2026-07-05 14:10:54 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  // Ekdum simple setup bina kisi extra plugin ke
+  plugins: [],
 })
